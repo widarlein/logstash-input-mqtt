4536ec711e88e1ba6ffd51d53ec307f89eeafc04
Alexander Widar <alexander@widar.nu>
2015-06-07 23:30:06 +0200

    added first version functionality

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gemspec
 gem "logstash", :github => "elastic/logstash", :branch => "1.4"
+gem 'mqtt', '~> 0.3.1'
--- lib/logstash/inputs/mqtt.rb
+++ lib/logstash/inputs/mqtt.rb
@@ -3,6 +3,7 @@ require "logstash/inputs/base"
 require "logstash/namespace"
 require "stud/interval"
 require "socket" # for Socket.gethostname
+require "mqtt"
 
 # Generate a repeating message.
 #
@@ -21,18 +22,49 @@ class LogStash::Inputs::Mqtt < LogStash::Inputs::Base
   #
   # The default, `1`, means send a message every second.
   config :interval, :validate => :number, :default => 1
+  
+  # The host of the MQTT broker
+  config :mqttHost, :validate => :string, :default => "localhost", :required => true
+  
+  # The port that the MQTT broker is using
+  config :port, :validate => :number, :default => 1883, :required => true
+  
+  # Whether the MQTT broker is using SSL or not.
+  config :ssl, :validate => :boolean, :default => false
+  
+  # The topic that the plugin should subscribe to
+  config :topic, validate => :string, :required => true
 
   public
   def register
     @host = Socket.gethostname
+    @client = MQTT::Client.connect(
+        :host => @mqttHost,
+        :port => @port,
+        :ssl => @ssl
+        :client_id => MQTT::Client.generate_client_id("logstash-mqtt-input")
+    )
   end # def register
 
   def run(queue)
-    Stud.interval(@interval) do
-      event = LogStash::Event.new("message" => @message, "host" => @host)
-      decorate(event)
-      queue << event
-    end # loop
+#    Stud.interval(@interval) do
+#      event = LogStash::Event.new("message" => @message, "host" => @host)
+#      decorate(event)
+#      queue << event
+#    end # loop
+
+    @client.get do |topic,message|
+        #hej
+        @codec.decode(message) do |event|
+            event["host"] ||= @host
+            event["topic"] = topic
+            
+            decorate(event)
+            queue << event
+        end
+    end
+    
+    @client.subscribe(@topic)
   end # def run
 
 end # class LogStash::Inputs::Example
