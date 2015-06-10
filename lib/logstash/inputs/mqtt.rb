d457b55700ac54559a87bc87e9d68036ff397d38
Alexander Widar <alexander@widar.nu>
2015-06-10 22:32:02 +0200

    removed example code and improved comments

--- lib/logstash/inputs/mqtt.rb
+++ lib/logstash/inputs/mqtt.rb
@@ -5,23 +5,14 @@ require "stud/interval"
 require "socket" # for Socket.gethostname
 require "mqtt"
 
-# Generate a repeating message.
-#
-# This plugin is intented only as an example.
+
+# Receive events from a MQTT topic
 
 class LogStash::Inputs::Mqtt < LogStash::Inputs::Base
   config_name "mqtt"
 
-  # If undefined, Logstash will complain, even if codec is unused.
+  # The codec used for input data. Input codecs are a convenient method for decoding your data before it enters the input, without needing a separate filter in your Logstash pipeline.
   default :codec, "plain" 
-
-  # The message string to use in the event.
-  config :message, :validate => :string, :default => "Hello World!"
-
-  # Set how frequently messages should be sent.
-  #
-  # The default, `1`, means send a message every second.
-  config :interval, :validate => :number, :default => 1
   
   # The host of the MQTT broker
   config :mqttHost, :validate => :string, :default => "localhost", :required => true
@@ -29,7 +20,7 @@ class LogStash::Inputs::Mqtt < LogStash::Inputs::Base
   # The port that the MQTT broker is using
   config :port, :validate => :number, :default => 1883, :required => true
   
-  # Whether the MQTT broker is using SSL or not.
+  # Whether connection to the MQTT broker is using SSL or not.
   config :ssl, :validate => :boolean, :default => false
   
   # The topic that the plugin should subscribe to
@@ -47,16 +38,9 @@ class LogStash::Inputs::Mqtt < LogStash::Inputs::Base
   end # def register
 
   def run(queue)
-#    Stud.interval(@interval) do
-#      event = LogStash::Event.new("message" => @message, "host" => @host)
-#      decorate(event)
-#      queue << event
-#    end # loop
-    puts @topic
+
     @client.subscribe(@topic)
     @client.get do |topic,message|
-        #hej
-        puts "message was received #{message}"
         @codec.decode(message) do |event|
             event["host"] ||= @host
             event["topic"] = topic
@@ -65,8 +49,6 @@ class LogStash::Inputs::Mqtt < LogStash::Inputs::Base
             queue << event
         end
     end
-    
-    
   end # def run
 
 end # class LogStash::Inputs::Example
